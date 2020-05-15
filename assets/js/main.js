987bd4fb08bef9a46e7b9416c836894561f946d6
jibonkhandakar6274 <jibonhandakar.jk@gmail.com>
2020-05-15 16:42:37 +0600

    full responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function ($) {
         loop: true,
         autoplay: true,
         autoplayTimeout: 2000,
+        margin: 0,
         smartSpeed: 300,
         responsive: {
             0: {
@@ -13,8 +14,30 @@ $(document).ready(function ($) {
                 items: 3
             },
             1000: {
-                items: 6
+                items: 4
             }
         }
     });
+    $('.counter').each(function () {
+        var $this = $(this),
+          countTo = $this.attr('data-count');
+  
+        $({ countNum: $this.text() }).animate({
+          countNum: countTo
+        },
+  
+          {
+  
+            duration: 3000,
+            easing: 'linear',
+            step: function () {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function () {
+              $this.text(this.countNum);
+              //alert('finished');
+            }
+  
+          });
+      });
 }(jQuery));
